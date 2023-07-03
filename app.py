12840908f1cd10b730dee4cc56be4b485cc1cfe8
keonm <artificialcheeto@gmail.com>
2023-07-03 12:21:32 -0500

    Added local files to repository

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return "Hello"
+	return render_template('login.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')    
+    
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html') 
+
+
+@app.route('/map')
+def map():
+    return render_template('map.html')    
+
+
+@app.route('/forgotpassword')
+def forgotpassword():
+    return render_template('forgotpassword.html')    
+
 
 if __name__ == 'main':
 	app.run(debug=True)
---
+++ static/mapscript.js
@@ -0,0 +1,7 @@
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8,
+        mapId: 'd4e922012220403a'
+    });
+}
